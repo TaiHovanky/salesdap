9c7edb35be1039b0f4cd890d8d4a6ff5290c09d8
Tai Hovanky <tai.hovanky@gmail.com>
2022-04-20 11:33:29 -0500

    builds

--- client/src/containers/profile/index.tsx
+++ client/src/containers/profile/index.tsx
@@ -72,7 +72,7 @@ const ProfileContainer = ({
         user.email
       );
     }
-    axios.post('http://localhost:3001/api/v1/pinfile', formData)
+    axios.post('/api/v1/pinfile', formData)
       .then((res: any) => {
         pinFileSuccess(res.data);
         hideError();
