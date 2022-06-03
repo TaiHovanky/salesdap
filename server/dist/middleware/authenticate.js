2c5af016de5aa572ee3e2b5268f29a6da7903835
Tai Hovanky <tai.hovanky@gmail.com>
2022-06-03 03:04:15 -0500

    console log

--- server/dist/middleware/authenticate.js
+++ server/dist/middleware/authenticate.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.authenticate = void 0;
 const authenticate = (req, _, next) => {
-    console.log('req session', req.session);
+    console.log('req session ---- authenticate', req.session, req.sessionID);
     if (!req.session || !req.session.user) {
         const err = new Error('Unauthenticated');
         next(err);
--- server/middleware/authenticate.ts
+++ server/middleware/authenticate.ts
@@ -1,5 +1,5 @@
 export const authenticate = (req: any, _: any, next: any) => {
-  console.log('req session', req.session);
+  console.log('req session ---- authenticate', req.session, req.sessionID);
   if (!req.session || !req.session.user) {
     const err = new Error('Unauthenticated');
     // err.statusCode = 401;
