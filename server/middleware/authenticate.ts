4a5f004948af4930a0c0d95d4eeb407243637d27
Tai Hovanky <tai.hovanky@gmail.com>
2022-06-28 06:54:48 -0500

    access token

--- server/middleware/authenticate.ts
+++ server/middleware/authenticate.ts
@@ -3,12 +3,13 @@ import { verify } from 'jsonwebtoken';
 export const authenticate = (req: any, _: any, next: any) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
+    console.log('no auth headers---------------------------------------')
     const err = new Error('Unauthenticated');
     next(err);
   }
   try {
     const token = authorization.split(' ')[1];
-    const payload = verify(token, process.env.REFRESH_TOKEN_SECRET as string);
+    const payload = verify(token, process.env.ACCESS_TOKEN_SECRET as string);
     if (payload) {
       return next();
     }
